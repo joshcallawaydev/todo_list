e3b577e310b727dc21ee6574792c04693eeca181
joshcallawaydev <joshrcal89@gmail.com>
2021-06-04 17:45:50 +0000

    Grab html elements.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,11 @@
 
 // grab html elements here
 
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
 // add todo function needed
 
 // delete todo function needed
