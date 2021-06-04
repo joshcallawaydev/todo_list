748f5033b40c2fb2d10d2a8cb5a51874c364f3d7
joshcallawaydev <joshrcal89@gmail.com>
2021-06-04 17:47:52 +0000

    Added event listners and created add todo function.

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,39 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
+document.addEventListener('DOMContentLoaded', getTodos)
+todoButton.addEventListener("click", addTodo); // Event listener
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
 // add todo function needed
+
+function addTodo(event) {
+    event.preventDefault(); // Prevents my form from submitting
+
+    const todoDiv = document.createElement("div"); // Creates the ToDo div tag and adds class
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement("li"); // Creates the list item and adds class 
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    saveLocalTodos(todoInput.value)
+
+    const completedButton = document.createElement("button"); // Adds check button
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv); // Append to list
+
+    todoInput.value = ""; // clears to do input box
+}
 
 // delete todo function needed
 
