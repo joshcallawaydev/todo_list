0f969619f7460d26ed541cfc76995eb0de99c1f4
joshcallawaydev <joshrcal89@gmail.com>
2021-06-04 17:44:29 +0000

    road map for to do js functionality.

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+// what do we need?
+
+// grab html elements here
+
+// add todo function needed
+
+// delete todo function needed
+
+// filter todo??
+
+// get todos?
+
+// access + munipulate local storage?
+
